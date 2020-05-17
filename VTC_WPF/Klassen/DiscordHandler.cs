f8a8600682ccbfeaafd0b238cd1b23cbfc199f18
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-05-17 15:53:44 +0200

    some fixes

--- VTC_WPF/Klassen/DiscordHandler.cs
+++ VTC_WPF/Klassen/DiscordHandler.cs
@@ -4,28 +4,25 @@ namespace VTC_WPF.Klassen
 {
     internal class DiscordHandler
     {
-        private class Discord
+        private DiscordRpcClient client;
+
+        public DiscordHandler()
         {
-            private DiscordRpcClient client;
 
-            public Discord()
+            client = new DiscordRpcClient(Config.DiscordAppID);
+            client.Initialize();
+            client.SetPresence(new RichPresence()
             {
-
-                client = new DiscordRpcClient(Config.DiscordAppID);
-                client.Initialize();
-                client.SetPresence(new RichPresence()
+                Details = "Starte...",
+                Assets = new Assets()
                 {
-                    Details = "Starte...",
-                    Assets = new Assets()
-                    {
-                        LargeImageKey = Config.DiscordLargeImageKey,
-                        LargeImageText = "Beyond the limits",
-                        SmallImageKey = Config.DiscordSmallImageKey,
-                        SmallImageText = "VTCManager Version " + Config.ClientVersion
-                    }
+                    LargeImageKey = Config.DiscordLargeImageKey,
+                    LargeImageText = "Beyond the limits",
+                    SmallImageKey = Config.DiscordSmallImageKey,
+                    SmallImageText = "v" + Config.ClientVersion
+                }
 
-                });
-            }
+            });
         }
     }
 }
