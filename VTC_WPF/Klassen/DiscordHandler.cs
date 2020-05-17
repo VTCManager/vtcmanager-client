c52cc0c4966c3729abef42f0b0d4936bd91091cf
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-05-17 15:46:19 +0200

    added DiscordRPC

---
+++ VTC_WPF/Klassen/DiscordHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordRPC;
+
+namespace VTC_WPF.Klassen
+{
+    class DiscordHandler
+    {
+        class Discord
+        {
+            private DiscordRpcClient client;
+
+            public Discord()
+            {
+
+                     client = new DiscordRpcClient(Information.DiscordAppID);
+                        client.Initialize();
+                        client.SetPresence(new RichPresence()
+                        {
+                            Details = "Starte...",
+                            Assets = new Assets()
+                            {
+                                LargeImageKey = Information.DiscordLargeImageKey,
+                                LargeImageText = "Beyond the limits",
+                                SmallImageKey = Information.DiscordSmallImageKey,
+                                SmallImageText = "VTCManager Version " + Information.ClientVersion
+                            }
+
+                        });
+            }
+        }
+    }
+}
