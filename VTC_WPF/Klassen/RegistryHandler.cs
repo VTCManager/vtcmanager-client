7fa58388fc68e1046eb6ad478835377fde16d91e
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-05-18 15:36:49 +0200

    added RegistryHandler

---
+++ VTC_WPF/Klassen/RegistryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VTC_WPF.Klassen
+{
+    class RegistryHandler
+    {
+        public static object read(string registryPath, string valueName)
+        {
+            try
+            {
+                return Registry.GetValue(registryPath, valueName, null);
+            }
+            catch (System.Security.SecurityException ex)
+            {
+                return null;
+            }
+        }
+    }
+}
