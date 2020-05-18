2c47ad51282a814f30839aecbce3143a838f2fea
Thommy <edv.blasius@gmail.com>
2020-05-18 08:46:26 +0200

    Update View. Added Traffic Area, Moved Speed, RPM etc. Label to Center

--- VTC_WPF/MainWindow.xaml.cs
+++ VTC_WPF/MainWindow.xaml.cs
@@ -12,13 +12,6 @@ namespace VTC_WPF
     {
         public SCSSdkTelemetry Telemetry;
         private readonly bool InvokeRequired;
-
-        public int Degrees { get; private set; }
-        public double Y2 { get; private set; }
-        public double X2 { get; private set; }
-        public int X1 { get; private set; }
-        public int Y1 { get; private set; }
-
         private delegate void UpdateProgressDelegate(DependencyProperty dp, object value);
         public DiscordHandler Discord;
         public JobHandler jobHandler;
@@ -48,52 +41,31 @@ namespace VTC_WPF
             try
             {
 
-                if (InvokeRequired)
-                {
-                    // Invoke((Delegate)new TelemetryData(this.Telemetry_Data), (object)data, (object)updated);
-                }
+                if (InvokeRequired) { }
                 else
                 {
-                    UpdateLabelContent(RPM_lbl, Convert.ToInt32(data.TruckValues.CurrentValues.DashboardValues.RPM).ToString() + " R/PM");
-                    UpdateLabelContent(speed_label_tacho, Convert.ToInt32(data.TruckValues.CurrentValues.DashboardValues.Speed.Kph).ToString());
-                    UpdateLabelContent(Speed_Limit_Label, Convert.ToInt32(data.NavigationValues.SpeedLimit.Kph).ToString());
-                    UpdateLabelContent(Speed_Limiter_Setting, Convert.ToInt32(data.TruckValues.CurrentValues.DashboardValues.CruiseControlSpeed.Kph).ToString());
+                        UpdateLabelContent(RPM_lbl, Convert.ToInt32(data.TruckValues.CurrentValues.DashboardValues.RPM).ToString() + " R/PM");
+                        UpdateLabelContent(speed_label_tacho, Convert.ToInt32(data.TruckValues.CurrentValues.DashboardValues.Speed.Kph).ToString());
+                        UpdateLabelContent(Speed_Limit_Label, Convert.ToInt32(data.NavigationValues.SpeedLimit.Kph).ToString());
+                        UpdateLabelContent(Speed_Limiter_Setting, Convert.ToInt32(data.TruckValues.CurrentValues.DashboardValues.CruiseControlSpeed.Kph).ToString());
 
-                    if (data.TruckValues.CurrentValues.MotorValues.GearValues.Selected == -1)
-                    {
-                        UpdateLabelContent(Gang_Label, "R");
-                    }
-
-                    UpdateLabelContent(Gang_Label, Convert.ToInt32(data.TruckValues.CurrentValues.MotorValues.GearValues.Selected).ToString());
-
+                        if (data.TruckValues.CurrentValues.MotorValues.GearValues.Selected == -1)
+                        {
+                            UpdateLabelContent(Gang_Label, "R");
+                        }
+                        UpdateLabelContent(Gang_Label, Convert.ToInt32(data.TruckValues.CurrentValues.MotorValues.GearValues.Selected).ToString());
                 }
-
             }
             catch
-            {
-
-            }
+            { }
+            
         }
 
         public void UpdateLabelContent(Label label, string newContent)
         {
-            Dispatcher.Invoke(new UpdateProgressDelegate(label.SetValue), DispatcherPriority.Background, ContentProperty, newContent);
+                Dispatcher.Invoke(new UpdateProgressDelegate(label.SetValue), DispatcherPriority.Background, ContentProperty, newContent);
         }
 
-        private void Rotate(float grad)
-        {
-            X1 = 0;
-            Y1 = 0;
-            double rad = Degrees * Math.PI / grad;
-            const int radius = 180;
-            double sin = Math.Sin(rad);
-            double cos = Math.Cos(rad);
-            double tan = Math.Tan(rad);
-
-            Y2 = sin * radius;
-            X2 = cos * radius;
-
-        }
 
     }
 }
