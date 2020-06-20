9fae449cfa9f9e4f9d05fc7d4e97f8f4fe5a4498
Thommy <edv.blasius@gmail.com>
2020-06-20 10:31:37 +0200

    Implement the Background Changer. Adding Button to Set Desktop Wallpaper to VTC. Todo: many more Wallpaper

--- VTC_WPF/Properties/Resources.Designer.cs
+++ VTC_WPF/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace VTCManager.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap Wallpaper1 {
+            get {
+                object obj = ResourceManager.GetObject("Wallpaper1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
